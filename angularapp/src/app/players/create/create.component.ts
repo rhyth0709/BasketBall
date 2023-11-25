ba78ee18541ac0e0b691e0c8f52483bde8d01063
Iamneo <techadmin@iamneo.ai>
2023-11-25 10:06:06 +0000

    Backup Commit

--- angularapp/src/app/players/create/create.component.ts
+++ angularapp/src/app/players/create/create.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Position } from 'src/app/models/position';
 import { PositionsService } from 'src/app/services/positions.service';
-
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
+import { PlayersService } from 'src/app/services/players.service';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -8,9 +10,36 @@ import { PositionsService } from 'src/app/services/positions.service';
 })
 export class CreateComponent implements OnInit {
 
-  constructor(private as :PositionsService) { }
+
+  positions: Position[]=[]
+  createForm : FormGroup
+  constructor(private as :PositionsService,private fb:FormBuilder,private ps:PlayersService) {
+
+    this.createForm = this.fb.group({
+      shirtno : ['',Validators.required],
+      name : ['',Validators.required],
+      positionid : ['',Validators.required],
+      appearances : ['',Validators.required],
+      goals : ['',Validators.required]  
+    })
+
+   }
+
+   onSubmit(formData:FormGroup):void{
+
+
+   }
+
+  
 
   ngOnInit(): void {
+
+    this.as.getPositions().subscribe((data)=>{
+      this.positions.push(...data)
+    })
+
+
+   
   }
 
 }
